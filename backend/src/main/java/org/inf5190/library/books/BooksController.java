5e9ca7e6d2f62d66ff40857cdac843cd8fd3a81b
Felix Trepanier <felixtrepanier@gmail.com>
2024-11-12 20:34:32 -0500

    fix meter names

--- backend/src/main/java/org/inf5190/library/books/BooksController.java
+++ backend/src/main/java/org/inf5190/library/books/BooksController.java
@@ -2,7 +2,8 @@ package org.inf5190.library.books;
 
 import java.util.List;
 import java.util.concurrent.ExecutionException;
-
+import org.inf5190.library.books.model.Book;
+import org.inf5190.library.books.repository.BooksRepository;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,10 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import org.inf5190.library.books.model.Book;
-import org.inf5190.library.books.repository.BooksRepository;
-
 import io.micrometer.core.annotation.Timed;
 
 @RestController
@@ -25,7 +22,7 @@ public class BooksController {
         this.booksRepository = booksRepository;
     }
 
-    @Timed(value = "com.inf5190.library.books.BooksController.get", percentiles = {0.5, 0.95, 0.99})
+    @Timed(value = "org.inf5190.library.books.BooksController.get", percentiles = {0.5, 0.95, 0.99})
     @GetMapping("/books")
     public List<Book> get(@RequestParam(name = "limit", defaultValue = "20") Integer limit,
             @RequestParam(name = "order", defaultValue = "asc") Order order)
@@ -33,13 +30,13 @@ public class BooksController {
         return this.booksRepository.getBooks(limit, order);
     }
 
-    @Timed(value = "com.inf5190.library.books.BooksController.add", percentiles = {0.5, 0.95, 0.99})
+    @Timed(value = "org.inf5190.library.books.BooksController.add", percentiles = {0.5, 0.95, 0.99})
     @PostMapping("/books")
     public Book add(@RequestBody Book book) throws InterruptedException, ExecutionException {
         return this.booksRepository.addBook(book);
     }
 
-    @Timed(value = "com.inf5190.library.books.BooksController.delete",
+    @Timed(value = "org.inf5190.library.books.BooksController.delete",
             percentiles = {0.5, 0.95, 0.99})
     @DeleteMapping("/books/{id}")
     public void delete(@PathVariable(name = "id") String id)
